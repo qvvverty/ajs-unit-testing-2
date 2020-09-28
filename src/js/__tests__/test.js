96885ac997eb0ce8ab3166270b8fc8516e739120
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-09-28 21:01:31 +0300

    sortHeroesByHealth improve, test fix

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -3,15 +3,15 @@ import sortHeroesByHealth from '../index';
 test('Expect heroes to be sorted by health', () => {
   const heroes = [
     { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
     { name: 'маг', health: 100 },
     { name: 'вор', health: 10 },
+    { name: 'мечник', health: 30 },
   ];
   const heroesExpected = [
     { name: 'маг', health: 100 },
     { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 30 },
     { name: 'вор', health: 10 },
-    { name: 'мечник', health: 10 },
   ];
   expect(sortHeroesByHealth(heroes)).toEqual(heroesExpected);
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 export default function sortHeroesByHealth(heroes) {
   function sortByHealth(a, b) {
-    if (a.health > b.health) return 1;
+    if (a.health > b.health) return -1;
     if (a.health === b.health) return 0;
-    return -1;
+    return 1;
   }
-  return heroes.sort(sortByHealth).reverse();
+  return heroes.sort(sortByHealth);
 }
